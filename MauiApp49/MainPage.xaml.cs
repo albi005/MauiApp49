4907ee68a8395d7ddb8bce925987d345249e66ed
Albert Ragány-Németh <raganyalbi@gmail.com>
2023-02-21 12:12:59 +0100

    Add project files.

---
+++ MauiApp49/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MauiApp49;
+
+public partial class MainPage : ContentPage
+{
+	private readonly Model[] _models = Enumerable.Range(0, 7).Select(i => new Model(i)).ToArray();
+
+	public MainPage()
+	{
+		InitializeComponent();
+		_collectionView.ItemsSource = _models;
+        _collectionView.Scrolled += Scrolled;
+	}
+
+    private void Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+		int first = e.FirstVisibleItemIndex;
+		int last = e.LastVisibleItemIndex;
+
+		_label.Text = $"first: {first}, last: {last}";
+
+		for (int i = 0; i < _models.Length; i++)
+		{
+			_models[i].IsVisible = i >= first && i <= last;
+		}
+    }
+}
+
+public partial class Model : ObservableObject
+{
+	[ObservableProperty, NotifyPropertyChangedFor(nameof(Color))]
+	private bool _isVisible;
+
+    public Model(int index) => Index = index;
+
+    public int Index { get; }
+	public Color Color => IsVisible ? Colors.LightGreen : Colors.PaleVioletRed;
+}
---
+++ MauiApp49/MauiProgram.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp49;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp() => MauiApp
+        .CreateBuilder()
+        .UseMauiApp<App>()
+        .Build();
+}
+
+public class App : Application
+{
+    public App() => MainPage = new MainPage();
+}
